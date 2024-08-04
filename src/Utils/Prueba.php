33cdf5a803efe45646e54746a1b4f89c0c379261
Johann Ramírez <johann.devfull@gmail.com>
2024-08-04 14:49:26 -0500

    Utils

---
+++ src/Utils/Prueba.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Johanncol\Notifications\Utils;
+
+
+
+class Prueba
+{
+
+  /**
+   * Execute the console command.
+   */
+  public function handle()
+  {
+    // $this->info("<bg=yellow;fg=black> JOHANN RAMÍREZ:: </> : 77777!");
+
+    return 'Hola mundo';
+  }
+
+}
